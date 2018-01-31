7b46d8d885c594dd786a3d0c3dbeb31425d9ac7c
iamchristopher <chriswright@live.com>
2018-01-30 20:25:02 -0500

    Moved speed and reaction distance variables to constants

--- src/components/states/StateChase.js
+++ src/components/states/StateChase.js
@@ -1,19 +1,22 @@
 import State from './State';
 import StatePatrol from './StatePatrol';
 
+const SPEED = 100;
+const REACTION_DISTANCE = 200;
+
 export default class StateChase extends State {
     onEnter () {
-        this.obj.body.accelGround = 200;
+        this.obj.body.accelGround = SPEED;
     }
 
     execute () {
         const distanceToPlayer = Phaser.Math.Distance.Between(this.obj.x, this.obj.y, window.player.x, window.player.y);
 
-        if (distanceToPlayer >= 200) {
+        if (distanceToPlayer >= REACTION_DISTANCE) {
             return this.obj.changeState(new StatePatrol(this.obj));
         }
 
-        const direction = this.obj.x - window.player.x > 0 ? -200 : 200;
+        const direction = this.obj.x - window.player.x > 0 ? -SPEED : SPEED;
         this.obj.body.accelGround = direction;
         this.obj.setVelocityX(this.obj.body.accelGround);
     }
--- src/components/states/StatePatrol.js
+++ src/components/states/StatePatrol.js
@@ -1,9 +1,12 @@
 import State from './State';
 import StateChase from './StateChase';
 
+const SPEED = 100;
+const REACTION_DISTANCE = 200;
+
 export default class StatePatrol extends State {
     onEnter () {
-        this.obj.body.accelGround = this.obj.body.accelGround > 0 ? 100 : -100;
+        this.obj.body.accelGround = this.obj.body.accelGround > 0 ? SPEED : -SPEED;
     }
 
     execute () {
@@ -11,12 +14,12 @@ export default class StatePatrol extends State {
         const nextTile = this.obj.scene.map.getTileAtWorldXY(this.obj.body.pos.x + nextX, this.obj.body.pos.y + 45);
 
         if (nextTile === null) {
-            this.obj.body.accelGround = this.obj.body.accelGround * -1;
+            this.obj.body.accelGround *= -1;
         }
 
         const distanceToPlayerX = Phaser.Math.Distance.Between(this.obj.x, this.obj.y, window.player.x, window.player.y);
 
-        if ((this.obj.y <= window.player.y + 16 && this.obj.y >= window.player.y - 16) && distanceToPlayerX < 200) {
+        if ((this.obj.y <= window.player.y + 16 && this.obj.y >= window.player.y - 16) && distanceToPlayerX < REACTION_DISTANCE) {
             return this.obj.changeState(new StateChase(this.obj));
         }
 
