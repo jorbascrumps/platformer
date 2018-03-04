d26093ef30e4fef332fb4b9d77c7d9dd6d136b44
iamchristopher <chriswright@live.com>
2018-03-04 18:17:58 -0500

    Added player healht is now rendered on UI layer

--- src/scenes/ui.js
+++ src/scenes/ui.js
@@ -2,9 +2,18 @@ export const key = 'ui';
 export const active = true;
 
 export function create () {
+    this.playerHealthTxt = this.add.text(1, 1);
     this.add.text(this.cameras.main.width - 2, this.cameras.main.height - 2, 'Alpha v1', {
         fontSize: 10,
         fontFamily: 'Arial'
     })
         .setOrigin(1, 1);
+}
+
+export function update () {
+    if (typeof window.players !== 'undefined') {
+        const playerHealth = window.players.getFirstAlive().health;
+
+        this.playerHealthTxt.setText(playerHealth);
+    }
 }
