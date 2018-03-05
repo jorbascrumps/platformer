bd42e0a1fd37698d5cdb3022e86d63bd5181dd91
iamchristopher <chriswright@live.com>
2018-03-04 19:55:10 -0500

    Added stroke to health text

--- src/scenes/ui.js
+++ src/scenes/ui.js
@@ -2,7 +2,12 @@ export const key = 'ui';
 export const active = true;
 
 export function create () {
-    this.playerHealthTxt = this.add.text(1, 1);
+    this.playerHealthTxt = this.add.text(1, 1, {
+        fontFamily: 'Arial',
+        fontSize: 14
+    })
+        .setStroke(0x000000, 3)
+        .setText(0);
     this.add.text(this.cameras.main.width - 2, this.cameras.main.height - 2, 'Alpha v1', {
         fontSize: 10,
         fontFamily: 'Arial'
