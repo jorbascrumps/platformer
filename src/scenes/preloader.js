e50b68f78d0e31f43051fae9fa06fc9ef7ed9b24
iamchristopher <chriswright@live.com>
2018-03-18 16:17:58 -0400

    Changed assets are now loaded from constants

--- src/scenes/preloader.js
+++ src/scenes/preloader.js
@@ -1,20 +1,19 @@
+import {
+    IMAGES,
+    JSON,
+    SPRITESHEETS
+} from '@/constants/files';
+
 export const key = 'preloader';
+
 export const loader = {
     path: 'src/data'
 };
 
 export function preload () {
-    this.load.image('tiles', 'tiles.png');
-    this.load.image('player', 'nega_nathan.png');
-    this.load.spritesheet('enemyWalk', 'enemy/walk.png', { frameWidth: 22, frameHeight: 33, endFrame: 12 });
-    this.load.spritesheet('enemyAttack', 'enemy/attack.png', { frameWidth: 43, frameHeight: 37, endFrame: 18 });
-    this.load.spritesheet('enemyIdle', 'enemy/idle.png', { frameWidth: 24, frameHeight: 32, endFrame: 10 });
-    this.load.spritesheet('enemyHit', 'enemy/hit.png', { frameWidth: 30, frameHeight: 32, endFrame: 9 });
-
-    this.load.json('left', 'left.json');
-    this.load.json('right', 'right.json');
-    this.load.json('down', 'down.json');
-    this.load.json('start', 'start.json');
+    this.load.image(IMAGES);
+    this.load.json(JSON);
+    this.load.spritesheet(SPRITESHEETS);
 
     this.progressBar = this.add.graphics();
     this.progressValue = this.add.text(this.cameras.main.width - 10, (this.cameras.main.height / 2) + 5, '0%', {
