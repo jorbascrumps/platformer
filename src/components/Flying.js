452b1fea441cb0ac0417833f7f74dfbcb12762a6
iamchristopher <chriswright@live.com>
2018-04-20 14:01:37 -0400

    Added flying enemy component

---
+++ src/components/Flying.js
@@ -0,0 +1,53 @@
+export default class Flying extends Phaser.Physics.Impact.Sprite {
+    constructor (scene, x, y) {
+        super(scene.impact.world, x, y, 'enemyWalk');
+
+        scene.sys.displayList.add(this); // TODO: Remove in favour of Group
+        scene.sys.updateList.add(this); // TODO: Remove in favour of Group
+
+        this.setGravity(0);
+        this.setBodySize(10, 23);
+
+        this.update.bind(this);
+    }
+
+    preUpdate (...args) {
+        this.update(...args);
+    }
+
+    update () {
+        const {
+            body: {
+                pos: {
+                    x: playerX,
+                    y: playerY
+                }
+            }
+        } = window.players.getFirstAlive();
+
+        const distance = Phaser.Math.Distance.Between(
+            playerX,
+            playerY,
+            this.body.pos.x,
+            this.body.pos.y - 20
+        );
+
+        if (distance < 200) {
+            const rotation = Phaser.Math.Angle.Between(
+                playerX,
+                playerY,
+                this.body.pos.x,
+                this.body.pos.y + 20
+            );
+
+            this.setVelocity(
+                -Math.cos(rotation) * 60,
+                -Math.sin(rotation) * 60
+            );
+
+            this.setRotation(Phaser.Math.Angle.Normalize(rotation));
+        } else {
+            this.setVelocity(0, 0);
+        }
+    }
+}
