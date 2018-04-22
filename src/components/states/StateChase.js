a4eee4f8d0ddddb24ee10c3a943c8f1910517665
iamchristopher <chriswright@live.com>
2018-04-22 13:31:20 -0400

    Code cleanup

--- src/components/states/StateChase.js
+++ src/components/states/StateChase.js
@@ -19,21 +19,23 @@ export default class StateChase extends State {
             obj: self
         } = this;
         const player = players.getFirstAlive();
-        const distanceToPlayer = Phaser.Math.Distance.Between(self.x, self.y, player.x, player.y);
 
-        if (!self.canSeePlayer || distanceToPlayer >= REACTION_DISTANCE) {
+        const distanceToPlayer = Phaser.Math.Distance.Between(self.x, self.y, player.x, player.y);
+        const isWithinChasingDistance = distanceToPlayer < REACTION_DISTANCE;
+
+        if (!self.canSeePlayer || !isWithinChasingDistance) {
             return self.emit(STATE_CHANGE, self.previousState);
         }
 
         const direction = self.x - player.x > 0 ? -SPEED : SPEED;
+
+        self.body.accelGround = direction;
+        self.flipX = direction < 0;
         self.body.offset = {
-            x: direction < 0 ? 11 : 2,
+            x: self.flipX ? 11 : 2,
             y: 9
         };
 
-        self.flipX = direction < 0;
-
-        self.body.accelGround = direction;
         self.setVelocityX(self.body.accelGround);
     }
 }
