fda5869e510a52eba0627b638a6fc05e3276b01b
iamchristopher <chriswright@live.com>
2018-09-30 18:40:35 -0400

    WIP Updated StateJump to use new Player

--- src/components/states/StateJump.js
+++ src/components/states/StateJump.js
@@ -4,49 +4,31 @@ import {
     STATE_CHANGE
 } from '@/constants/events';
 
+const VELOCITY_JUMP = -7;
+
 export default class StateJump extends State {
     onEnter () {
         super.onEnter();
 
-        const {
-            obj: self
-        } = this;
-        const {
-            scene: {
-                cursors
-            }
-        } = self;
-
-        if (cursors.left.isDown) {
-            self.flipX = true;
-        } else if (cursors.right.isDown) {
-            self.flipX = false;
-        }
-
-        self.body.offset = {
-            x: self.flipX ? 11 : 2,
-            y: 9
-        };
-
-        self.setVelocityY(-650);
+        this.target.sprite.setVelocityY(VELOCITY_JUMP);
     }
 
     execute () {
         const {
-            obj: self
+            target: {
+                scene: {
+                    cursors
+                }
+            },
+            target
         } = this;
-        const {
-            scene: {
-                cursors
-            }
-        } = self;
 
-        if (self.body.standing) {
-            return self.emit(STATE_CHANGE, self.previousState);
+        if (target.isTouching.ground) {
+            return target.events.emit(STATE_CHANGE, target.previousState);
         }
 
         if (cursors.left.isDown || cursors.right.isDown) {
-            return self.emit(STATE_CHANGE, StateWalk);
+            return target.events.emit(STATE_CHANGE, StateWalk);
         }
     }
 }
