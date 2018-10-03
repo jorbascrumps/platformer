55021fc760e87f7d818ac747ba09af7f6086fba0
iamchristopher <chriswright@live.com>
2018-10-02 20:23:00 -0400

    Updated StateLadder to work with new Player

--- src/components/states/StateLadder.js
+++ src/components/states/StateLadder.js
@@ -5,50 +5,51 @@ import {
 } from '@/constants/events';
 
 export default class StateLadder extends State {
+
     onEnter () {
         super.onEnter();
 
-        this.obj.setGravity(0);
+        this.target.sprite
+            .setIgnoreGravity(true)
+            .setVelocity(0);
     }
 
     execute () {
         const {
-            obj: {
+            target: {
                 scene: {
                     cursors
                 }
-            }
+            },
+            target
         } = this;
-        const groundTilePos = {
-            x: this.obj.body.pos.x + (this.obj.body.size.x / 2),
-            y: this.obj.body.pos.y + this.obj.body.size.y
-        };
-        const currentTileAtPosition = this.obj.scene.interactions.getTileAtWorldXY(groundTilePos.x, groundTilePos.y - 1, true);
 
-        if (currentTileAtPosition.index !== 11) {
-            return this.obj.emit(STATE_CHANGE, StateIdle);
+        if (!target.isTouching.ladder) {
+            return target.events.emit(STATE_CHANGE, StateIdle);
         }
 
         if (cursors.up.isDown) {
-            this.obj.setVelocityY(-200);
+            target.sprite.setVelocityY(-2.5);
         } else if (cursors.down.isDown) {
-            this.obj.setVelocityY(200);
+            target.sprite.setVelocityY(2.5);
         } else {
-            this.obj.setVelocityY(0);
+            target.sprite.setVelocityY(0);
         }
 
         if (cursors.left.isDown) {
-            this.obj.setVelocityX(-200);
+            target.sprite.setVelocityX(-2.5);
         } else if (cursors.right.isDown) {
-            this.obj.setVelocityX(200);
+            target.sprite.setVelocityX(2.5);
         } else {
-            this.obj.setVelocityX(0);
+            target.sprite.setVelocityX(0);
         }
     }
 
     onExit () {
         super.onExit();
 
-        this.obj.setGravity(1);
+        this.target.sprite
+            .setIgnoreGravity(false);
     }
+
 }
