3ca1b1f7a76836113fb6db140035b057ba40dffc
iamchristopher <chriswright@live.com>
2018-10-05 20:13:05 -0400

    Updated Enemy to work with new Actor class

--- src/components/enemy.js
+++ src/components/enemy.js
@@ -1,86 +1,87 @@
+import Actor from './Actor';
 import StatePatrol from './states/StatePatrol';
 import {
     DAMAGE_RECEIVE,
     STATE_CHANGE
 } from '@/constants/events';
 
-export default class Enemy extends Phaser.Physics.Impact.Sprite {
+const {
+    Physics: {
+        Matter: {
+            Matter: {
+                Body,
+                Bodies
+            }
+        }
+    }
+} = Phaser;
+
+export default class Enemy extends Actor {
+
     constructor (scene, x, y) {
-        super(scene.impact.world, x, y, 'enemyWalk');
+        super(scene, x, y);
 
-        this.update.bind(this);
-        this.changeState.bind(this);
-
-        this.on(STATE_CHANGE, this.changeState);
-
-        this.setOrigin(0, 0);
-        this.setMaxVelocity(500);
-        this.setFriction(2000, 100);
-        this.setBodySize(10, 23);
-        this.setActiveCollision();
-        this.setAvsB();
-        this.setCollideCallback(this.collide, this);
-
-        this.canSeePlayer = false;
-        this.body.accelGround = Phaser.Math.RND.between(-1, 1);
-
-        this.tint = 0xce4a4a;
-        this.body.offset = {
-            x: 2,
-            y: 9
+        const sensorOptions = {
+            isSensor: true
         };
+        const sprite = this.scene.matter.add.sprite(0, 0, 'player', 0)
+            .setDisplaySize(16, 32)
+            .setSize(16, 32);
+        const mainBody = Bodies.rectangle(0, 0, sprite.width, sprite.height, {
+            chamfer: {
+                radius: 4
+            }
+        });
+        this.sensors = {
+            right: Bodies.rectangle(sprite.width * 0.5, 0, 2, sprite.height * 0.5, sensorOptions),
+            bottom: Bodies.rectangle(0, sprite.height * 0.5, sprite.width * 0.25, 2, sensorOptions),
+            left: Bodies.rectangle(-sprite.width * 0.5, 0, 2, sprite.height * 0.5, sensorOptions)
+        };
+        const compoundBody = Body.create({
+            parts: [
+                mainBody,
+                ...Object.values(this.sensors)
+            ],
+            frictionStatic: 0,
+            frictionAir: 0.02,
+            friction: 0.05,
+            render: {
+                sprite: {
+                    xOffset: 1,
+                    yOffset: 0.3
+                }
+            }
+        });
+        this.sprite = sprite
+            .setExistingBody(compoundBody)
+            .setFixedRotation()
+            .setPosition(x, y)
+            .setTint(0xce4a4a);
 
-        this.previousState = null;
-        this.emit(STATE_CHANGE, StatePatrol);
+        this.events.emit(STATE_CHANGE, StatePatrol);
     }
 
-    collide (enemy, player) {
-        const {
-            gameObject: playerObject
-        } = player;
+    // update () {
+    //     const player = window.players.getFirstAlive();
+    //     const line = new Phaser.Geom.Line(this.body.pos.x, this.body.pos.y, player.body.pos.x, player.body.pos.y);
+    //
+    //     const x = this.body.pos.x > player.body.pos.x
+    //         ?   player.body.pos.x
+    //         :   this.body.pos.x;
+    //     const y = this.body.pos.y > player.body.pos.y
+    //         ?   player.body.pos.y
+    //         :   this.body.pos.y;
+    //     const w = this.body.pos.x > player.body.pos.x
+    //         ?   this.body.pos.x - player.body.pos.x
+    //         :   player.body.pos.x;
+    //     const h = this.body.pos.y > player.body.pos.y
+    //         ?   this.body.pos.y - player.body.pos.y
+    //         :   player.body.pos.y - this.body.pos.y;
+    //     this.canSeePlayer = !this.scene.ground.getTilesWithinWorldXY(x, y, w, h, {
+    //             isNotEmpty: true
+    //         })
+    //         .filter(tile => Phaser.Geom.Intersects.LineToRectangle(line, tile.getBounds()))
+    //         .length;
+    // }
 
-        playerObject.emit(DAMAGE_RECEIVE, 1);
-    }
-
-    preUpdate (...args) {
-        this.update(...args);
-    }
-
-    update () {
-        if (typeof this.state !== 'undefined') {
-            this.state.execute(this);
-        }
-
-        const player = window.players.getFirstAlive();
-        const line = new Phaser.Geom.Line(this.body.pos.x, this.body.pos.y, player.body.pos.x, player.body.pos.y);
-
-        const x = this.body.pos.x > player.body.pos.x
-            ?   player.body.pos.x
-            :   this.body.pos.x;
-        const y = this.body.pos.y > player.body.pos.y
-            ?   player.body.pos.y
-            :   this.body.pos.y;
-        const w = this.body.pos.x > player.body.pos.x
-            ?   this.body.pos.x - player.body.pos.x
-            :   player.body.pos.x;
-        const h = this.body.pos.y > player.body.pos.y
-            ?   this.body.pos.y - player.body.pos.y
-            :   player.body.pos.y - this.body.pos.y;
-        this.canSeePlayer = !this.scene.ground.getTilesWithinWorldXY(x, y, w, h, {
-                isNotEmpty: true
-            })
-            .filter(tile => Phaser.Geom.Intersects.LineToRectangle(line, tile.getBounds()))
-            .length;
-    }
-
-    changeState (state) {
-        if (this.state) {
-            this.state.onExit(this);
-        }
-
-        this.previousState = this.state;
-        this.state = typeof state === 'function'
-            ?   new state(this)
-            :   new state.constructor(this);
-    }
 }
