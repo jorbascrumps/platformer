ce8db3cc7993995c6e32fdf343326071d62fefd3
iamchristopher <chriswright@live.com>
2018-10-05 20:14:49 -0400

    Updated StatePatrol to work with new Enemy

--- src/components/states/StatePatrol.js
+++ src/components/states/StatePatrol.js
@@ -4,46 +4,67 @@ import {
     STATE_CHANGE
 } from '@/constants/events';
 
-const SPEED = 50;
 const REACTION_DISTANCE = 200;
+const SPEED_WALK = 0.0005;
+const SPEED_RUN = 0.005;
+const VELOCITY_MAX_WALK = 4;
+const VELOCITY_MAX_RUN = 5;
 
 export default class StatePatrol extends State {
-    onEnter () {
-        super.onEnter();
-
-        this.obj.body.accelGround = this.obj.body.accelGround > 0 ? SPEED : -SPEED;
-        this.obj.anims.play('enemyWalk');
-    }
 
     execute () {
         const {
-            obj: self
+            target: {
+                scene: {
+                    ground,
+                    player
+                }
+            },
+            target
         } = this;
-        const player = players.getFirstAlive();
+        const {
+            direction = 1
+        } = target.data.getAll();
 
-        const distanceToPlayerX = Phaser.Math.Distance.Between(self.x, self.y, player.x, player.y);
-        const isWithinChasingDistance = distanceToPlayerX < REACTION_DISTANCE;
+        const distanceToPlayerX = Phaser.Math.Distance.Between(
+            target.sprite.x + (target.sprite.displayWidth / 2),
+            target.sprite.y + (target.sprite.displayHeight / 2),
+            player.sprite.x,
+            player.sprite.y
+        );
+        const isWithinChasingDistance = distanceToPlayerX <= REACTION_DISTANCE;
 
-        if (this.obj.canSeePlayer && isWithinChasingDistance && (player.y >= self.y && self.y + self.height >= player.y)) {
-            return self.emit(STATE_CHANGE, StateChase);
+        if (isWithinChasingDistance) {
+            return target.sprite.setVelocityX(0);
         }
 
-        const nextX = this.obj.body.pos.x + (this.obj.body.accelGround > 1 ? this.obj.body.size.x + 1 : -1);
-        const nextY = this.obj.body.pos.y + this.obj.body.size.y + 1;
-        const nextGroundTile = this.obj.scene.ground.getTileAtWorldXY(nextX, nextY, true);
-        const nextAirTile = this.obj.scene.ground.getTileAtWorldXY(nextX, nextY - (this.obj.body.size.y / 2), true);
+        // if (this.obj.canSeePlayer && isWithinChasingDistance && (player.y >= self.y && self.y + self.height >= player.y)) {
+        //     return self.emit(STATE_CHANGE, StateChase);
+        // }
+
+        const nextX = target.sprite.x + (target.sprite.displayWidth / 2 * direction);
+        const nextY = target.sprite.y + (target.sprite.displayHeight / 2);
+        const nextGroundTile = ground.getTileAtWorldXY(nextX, nextY, true);
+        const nextAirTile = ground.getTileAtWorldXY(nextX, nextY - 1, true);
 
         if ((nextGroundTile === null || !nextGroundTile.collides) || (nextAirTile === null || nextAirTile.collides)) {
-            self.body.accelGround *= -1;
+            target.sprite.setVelocity(0);
+
+            return target.data.set('direction', direction * -1);
         }
 
-        this.obj.flipX = this.obj.body.accelGround < 0;
+        target.sprite.applyForce({ x: SPEED_WALK * direction, y: 0 });
 
-        this.obj.body.offset = {
-            x: this.obj.flipX ? 11 : 2,
-            y: 9
-        };
+        const maxVelocity = target.isTouchingGround
+            ?   VELOCITY_MAX_WALK
+            :   VELOCITY_MAX_WALK / 3;
+        const minVlocity = -maxVelocity;
 
-        return self.setVelocityX(self.body.accelGround);
+        if (target.sprite.body.velocity.x > maxVelocity) {
+            target.sprite.setVelocityX(maxVelocity);
+        } else if (target.sprite.body.velocity.x < minVlocity) {
+            target.sprite.setVelocityX(minVlocity);
+        }
     }
+
 }
