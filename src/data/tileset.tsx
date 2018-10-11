325e9fb7b3b77ab0066c5fb5eeddc756d47b53a6
iamchristopher <chriswright@live.com>
2018-10-10 20:48:08 -0400

    Changed map rooms now share an external tileset

---
+++ src/data/tileset.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="platformertiles" tilewidth="32" tileheight="32" tilecount="24" columns="8">
+ <image source="tiles.png" trans="ff00ff" width="256" height="96"/>
+ <tile id="9">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="index" type="int" value="101"/>
+   <property name="isClimbable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+   <property name="index" type="int" value="100"/>
+  </properties>
+ </tile>
+</tileset>
