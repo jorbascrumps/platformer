9d9a9c3090a24d2f6266ab7ccd30687ac9e821ac
iamchristopher <chriswright@live.com>
2018-10-23 16:17:42 -0400

    Updated Flying enemy to use Actor

--- src/components/Flying.js
+++ src/components/Flying.js
@@ -1,62 +1,91 @@
-export default class Flying extends Phaser.Physics.Impact.Sprite {
-    constructor (scene, x, y) {
-        super(scene.impact.world, x, y, 'enemyWalk');
+import Actor from './Actor';
+import ActorSprite from './ActorSprite';
+import StateSleep from './states/StateSleep';
+import {
+    STATE_CHANGE,
+} from '@/constants/events';
 
-        scene.sys.displayList.add(this); // TODO: Remove in favour of Group
-        scene.sys.updateList.add(this); // TODO: Remove in favour of Group
-
-        this.setActiveCollision();
-        this.setAvsB();
-        this.setGravity(0);
-        this.setBodySize(10, 10);
-        this.setOrigin(0.5, 0.5);
-
-        this.tint = 0xce4a4a;
-        this.body.offset = {
-            x: 5,
-            y: 16
-        };
-
-        this.update.bind(this);
+const {
+    Physics: {
+        Matter: {
+            Matter: {
+                Body,
+                Bodies
+            }
+        }
     }
+} = Phaser;
 
-    preUpdate (...args) {
-        this.update(...args);
+export default class Flying extends Actor {
+
+    constructor (scene, x, y) {
+        super(scene, x, y);
+
+        const sensorOptions = {
+            isSensor: true
+        };
+        const sprite = new ActorSprite(scene.matter.world, 0, 0, 'enemyFlying', 0)
+            .setActor(this)
+            .setDisplaySize(32, 32)
+            .setSize(32, 16);
+        const mainBody = Bodies.rectangle(0, 0, 16, 16, {
+            chamfer: {
+                radius: 4
+            }
+        });
+        this.sensors = {
+            right: Bodies.rectangle(8, 0, 2, sprite.height * 0.5, sensorOptions),
+            bottom: Bodies.rectangle(0, sprite.height * 0.5, sprite.width * 0.25, 2, sensorOptions),
+            left: Bodies.rectangle(-8, 0, 2, sprite.height * 0.5, sensorOptions)
+        };
+        const compoundBody = Body.create({
+            parts: [
+                mainBody,
+                ...Object.values(this.sensors)
+            ],
+            frictionStatic: 0,
+            frictionAir: 0.02,
+            friction: 0.05,
+            render: {
+                sprite: {
+                    xOffset: 0,
+                    yOffset: 0.1
+                }
+            }
+        });
+        this.sprite = sprite
+            .setExistingBody(compoundBody)
+            .setFixedRotation()
+            .setIgnoreGravity(true)
+            .setPosition(x, y)
+            .setTint(0xce4a4a);
+
+        this.events.emit(STATE_CHANGE, StateSleep);
     }
 
     update () {
+        super.update();
+
         const {
-            body: {
-                pos: {
-                    x: playerX,
-                    y: playerY
-                }
+            scene: {
+                ground,
+                player,
             }
-        } = window.players.getFirstAlive();
+        } = this;
 
-        const distance = Phaser.Math.Distance.Between(
-            playerX,
-            playerY,
-            this.body.pos.x,
-            this.body.pos.y - 20
-        );
+        const line = new Phaser.Geom.Line(this.sprite.x, this.sprite.y, player.sprite.x, player.sprite.y);
+        this.canSeePlayer = ground.getTilesWithinShape(line, {
+            isNotEmpty: true
+        })
+            .length === 0;
 
-        if (distance < 200) {
-            const rotation = Phaser.Math.Angle.Between(
-                playerX,
-                playerY,
-                this.body.pos.x,
-                this.body.pos.y + 20
-            );
-
-            this.setVelocity(
-                -Math.cos(rotation) * 60,
-                -Math.sin(rotation) * 60
-            );
-
-            this.setRotation(Phaser.Math.Angle.Normalize(rotation));
-        } else {
-            this.setVelocity(0, 0);
+        if (this.debug) {
+            const lineColour = this.canSeePlayer ? this.debugGraphic.defaultStrokeColor : 0xff0000;
+            this.debugGraphic
+                .clear()
+                .lineStyle(1, lineColour)
+                .strokeLineShape(line)
         }
     }
+
 }
